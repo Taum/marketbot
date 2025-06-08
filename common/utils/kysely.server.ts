461c64e88360af2c081842bb9612d4279041e293
Thomas Brian <taum.xd@gmail.com>
2025-06-08 17:04:13 -0400

    update kysely connector to use DATABASE_URL environment variable

--- common/utils/kysely.server.ts
+++ common/utils/kysely.server.ts
@@ -2,15 +2,56 @@ import { DB } from '@generated/kysely-db/types' // this is the Database interfac
 import { Pool } from 'pg'
 import { Kysely, PostgresDialect } from 'kysely'
 
+const dbUrl = process.env.DATABASE_URL
+
+
+// We hack compatibility with Prisma's DATABASE_URL format here, so we don't have to
+// change the container configurations.
+
+// example: postgresql://test:test@localhost:5411/test
+// example: postgresql://myUs3r:myP4ssw0rd@localhost:5432/dbName?host=/path/to/socket
+
+if (!dbUrl) {
+  throw new Error("DATABASE_URL is not set")
+}
+
+const url = new URL(dbUrl)
+if (!url) {
+  throw new Error("DATABASE_URL is not in the expected format")
+}
+
+if (url.protocol !== "postgresql:") {
+  throw new Error("DATABASE_URL protocol is not postgresql")
+}
+
+const user = url.username
+const password = url.password
+const host = url.hostname
+const port = url.port
+const database = url.pathname.slice(1)
+
+let params = {}
+if (url.searchParams.get("host")) {
+  params = {
+    user,
+    password,
+    database,
+    host: url.searchParams.get("host"),
+  }
+} else {
+  params = {
+    user,
+    password,
+    host,
+    port,
+    database,
+  }
+}
 
 
 const dialect = new PostgresDialect({
   pool: new Pool({
-    database: 'test',
-    host: 'localhost',
-    user: 'test',
-    password: 'test',
-    port: 5411,
+    ...params,
     max: 10,
   })
 })
